78e03faef08e7bc61e730c7347e6a10220cd16f7
Saiph <zhangwh754@gmail.com>
2021-11-07 13:12:26 +0800

    ✨ feat(index): 首页推荐歌单（未登录）

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,15 +4,21 @@ import { request } from "../../api/request";
 
 Page({
   data: {
-    banners: []
+    banners: [],
+    recommendList: []
   },
   // 页面渲染后执行
   onLoad: function (options) {
     // 1、请求首页轮播图数据
     request("banner", {}).then(res => {
-      console.log(res);
       this.setData({
         banners: res.banners,
+      });
+    });
+    // 2、请求首页推荐数据
+    request("personalized", {limit: 15}).then(res => {
+      this.setData({
+        recommendList: res.result,
       });
     });
   },
