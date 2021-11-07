7b306ad4a3f1360440372491459e27fdf70997b0
Saiph <zhangwh754@gmail.com>
2021-11-07 13:08:03 +0800

    ✨ feat(index): 首页轮播图和导航栏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,19 @@
 // index.js
-Page({});
+// 引用封装的request发送请求
+import { request } from "../../api/request";
+
+Page({
+  data: {
+    banners: []
+  },
+  // 页面渲染后执行
+  onLoad: function (options) {
+    // 1、请求首页轮播图数据
+    request("banner", {}).then(res => {
+      console.log(res);
+      this.setData({
+        banners: res.banners,
+      });
+    });
+  },
+});
