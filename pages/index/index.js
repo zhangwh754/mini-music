0eff1fcd8811302f28f4193239139fe28b6fa6c8
Saiph <zhangwh754@gmail.com>
2021-11-07 16:15:18 +0800

    ✨ feat(index): 首页歌曲分类排行榜

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,14 @@ import { request } from "../../api/request";
 Page({
   data: {
     banners: [],
-    recommendList: []
+    recommendList: [],
+    // 需要去请求的排行榜的id
+    idList: [19723756, 3779629, 2884035, 3778678, 1978921795],
+    topList: []
   },
   // 页面渲染后执行
   onLoad: function (options) {
+    const that = this
     // 1、请求首页轮播图数据
     request("banner", {}).then(res => {
       this.setData({
@@ -21,5 +25,20 @@ Page({
         recommendList: res.result,
       });
     });
+    // 3、请求排行榜数据，根据每个id去分别请求一次
+    const resultArr = []
+    that.data.idList.forEach(id => {
+      request("playlist/detail", {id}).then(res => {
+        const topListItem = {
+          id: res.playlist.id,
+          title: res.playlist.name,
+          songsList: res.playlist.tracks.slice(0, 3)
+        }
+        resultArr.push(topListItem)
+        this.setData({
+          topList: resultArr
+        });
+      });
+    })
   },
 });
