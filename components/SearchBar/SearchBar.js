a42bb950fb0897765611153666d7d8c0ecf0ecd9
Saiph <zhangwh754@gmail.com>
2021-11-08 12:00:46 +0800

    ✨ feat(components): 搜索栏组件

---
+++ components/SearchBar/SearchBar.js
@@ -0,0 +1,22 @@
+// components/SearchBar/SearchBar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    searchText: {
+      type: String,
+      value: "搜索您想找的音乐",
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
