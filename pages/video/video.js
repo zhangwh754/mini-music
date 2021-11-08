dece20db1f1a12475fe518687a48604939e9b266
Saiph <zhangwh754@gmail.com>
2021-11-08 13:43:11 +0800

    ✨ feat(video): 导航和搜索

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,66 +1,23 @@
 // pages/video/video.js
+
+import { request } from "../../api/request";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    navTitle: [],
+    currentIndex: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleSwitch(e) {
+    this.setData({
+      // 获取的自定义属性id是字符串，先转换为Number类型
+      currentIndex: Number(e.currentTarget.dataset.id)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function() {
+    request("video/group/list", {}).then(res => {
+      this.setData({
+        navTitle: res.data.slice(0, 8)
+      });
+    });
   }
 })
