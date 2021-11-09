68b9018c2bd1d7da7df5bc70e770c4aa1c9d69ae
Saiph <zhangwh754@gmail.com>
2021-11-09 15:58:07 +0800

    ✨ feat(recommendSong): 推荐歌曲展示

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -1,7 +1,11 @@
 // pages/recommendSong/recommendSong.js
+
+import { request } from "../../api/request";
+
 Page({
   data: {
-    date: {}
+    date: {},
+    songList: []
   },
 
   onLoad: function() {
@@ -11,6 +15,27 @@ Page({
         day: new Date().getDate(),
         month: new Date().getMonth() + 1
       }
+    })
+    // 获取cookie
+    const cookie = wx.getStorageSync('cookie')
+    // cookie不存在，跳转到login页面
+    if(!cookie) {
+      wx.showToast({
+        title: '请先登陆',
+        icon: 'error',
+        duration: 2000,
+        success: () => {
+          wx.reLaunch({
+            url: '/pages/login/login'
+          })
+        }
+      })
+    }
+    // 请求每日推荐歌曲
+    request('recommend/songs', {cookie}).then(res => {
+      this.setData({
+        songList: res.data.dailySongs
+      })
     })
   }
 })
